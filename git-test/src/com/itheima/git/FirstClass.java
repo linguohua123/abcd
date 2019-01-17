a1ef7e69c2351da617b0751efae61fecc86aa660
linguohua <asdf@itcast.cn>
2019-01-17 17:03:06 +0800

    git-test Project commiting

--- git-test/src/com/itheima/git/FirstClass.java
+++ git-test/src/com/itheima/git/FirstClass.java
@@ -8,5 +8,9 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
+
+        //由于，Idea的版本不同，需要在要提交的class右键-Git-Add
+        //然后再进行commit
+
     }
 }
